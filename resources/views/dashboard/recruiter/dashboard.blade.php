0ecba85fc0df5ff50744309ed2f3556d17a944dd
itsmushi <jmushi175@gmail.com>
2021-09-27 11:41:10 +0300

    display details dashboard recruiter

--- resources/views/dashboard/recruiter/dashboard.blade.php
+++ resources/views/dashboard/recruiter/dashboard.blade.php
@@ -2,161 +2,171 @@
 
 @section('content')
 
-<div class="clearfix"></div>
-<!-- Header Container / End -->
+    <div class="clearfix"></div>
+    <!-- Header Container / End -->
 
 
-<!-- Dashboard Container -->
-<div class="dashboard-container">
+    <!-- Dashboard Container -->
+    <div class="dashboard-container">
 
-	<!-- Dashboard Sidebar
-	================================================== -->
-	<div class="dashboard-sidebar">
-		<div class="dashboard-sidebar-inner" data-simplebar>
-			<div class="dashboard-nav-container">
+        <!-- Dashboard Sidebar
+        ================================================== -->
+        <div class="dashboard-sidebar">
+            <div class="dashboard-sidebar-inner" data-simplebar>
+                <div class="dashboard-nav-container">
 
-				<!-- Responsive Navigation Trigger -->
-				<a href="#" class="dashboard-responsive-nav-trigger">
-					<span class="hamburger hamburger--collapse" >
+                    <!-- Responsive Navigation Trigger -->
+                    <a href="#" class="dashboard-responsive-nav-trigger">
+					<span class="hamburger hamburger--collapse">
 						<span class="hamburger-box">
 							<span class="hamburger-inner"></span>
 						</span>
 					</span>
-					<span class="trigger-title">Dashboard Navigation</span>
-				</a>
-				
-				<!-- Navigation -->
-				<div class="dashboard-nav">
-					<div class="dashboard-nav-inner">
-						@include('includes.navigations.index')
-					</div>
-				</div>
-				<!-- Navigation / End -->
+                        <span class="trigger-title">Dashboard Navigation</span>
+                    </a>
 
-			</div>
-		</div>
-	</div>
-	<!-- Dashboard Sidebar / End -->
+                    <!-- Navigation -->
+                    <div class="dashboard-nav">
+                        <div class="dashboard-nav-inner">
+                            @include('includes.navigations.index')
+                        </div>
+                    </div>
+                    <!-- Navigation / End -->
 
-
-	<!-- Dashboard Content
-	================================================== -->
-	<div class="dashboard-content-container" data-simplebar>
-		<div class="dashboard-content-inner" >
-			
-			<!-- Dashboard Headline -->
-			<div class="dashboard-headline">
-				<h3>Hello, Employer!</h3>
-				<span>We are glad to see you again!</span>
-
-				<!-- Breadcrumbs -->
-				<nav id="breadcrumbs" class="dark">
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li>Dashboard</li>
-					</ul>
-				</nav>
-			</div>
-	
-			<!-- Fun Facts Container -->
-			<div class="fun-facts-container">
-				<div class="fun-fact" data-fun-fact-color="#36bd78">
-					<div class="fun-fact-text">
-						<span>Applicants</span>
-						<h4>22</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-person-pin"></i></div>
-				</div>
-				<div class="fun-fact" data-fun-fact-color="#b81b7f">
-					<div class="fun-fact-text">
-						<span>Active Jobs</span>
-						<h4>4</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-business-center"></i></div>
-				</div>
-				<div class="fun-fact" data-fun-fact-color="#efa80f">
-					<div class="fun-fact-text">
-						<span>Pending Jobs</span>
-						<h4>28</h4>
-					</div>
-					<div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
-				</div>
-			</div>
-			
-			<!-- Row -->
-			<div class="row">
-
-				<!-- Dashboard Box -->
-				<div class="col-xl-12">
-					<div class="dashboard-box">
-						<div class="headline">
-							<h3><i class="icon-material-baseline-notifications-none"></i> Notifications</h3>
-							<button class="mark-as-read ripple-effect-dark" data-tippy-placement="left" title="Mark all as read">
-									<i class="icon-feather-check-square"></i>
-							</button>
-						</div>
-						<div class="content">
-							<ul class="dashboard-box-list">
-								@include('dashboard.notifications.dashboard')
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- Row / End -->
-		</div>
-	</div>
-	<!-- Dashboard Content / End -->
-
-</div>
-<!-- Dashboard Container / End -->
-
-</div>
-<!-- Wrapper / End -->
+                </div>
+            </div>
+        </div>
+        <!-- Dashboard Sidebar / End -->
 
 
-<!-- Apply for a job popup
-================================================== -->
-<div id="small-dialog" class="zoom-anim-dialog mfp-hide dialog-with-tabs">
+        <!-- Dashboard Content
+        ================================================== -->
+        <div class="dashboard-content-container" data-simplebar>
+            <div class="dashboard-content-inner">
 
-	<!--Tabs -->
-	<div class="sign-in-form">
+                <!-- Dashboard Headline -->
+                <div class="dashboard-headline">
+                    <h3>Hello, Employer!</h3>
+                    <span>We are glad to see you again!</span>
 
-		<ul class="popup-tabs-nav">
-			<li><a href="#tab">Add Note</a></li>
-		</ul>
+                    <!-- Breadcrumbs -->
+                    <nav id="breadcrumbs" class="dark">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li>Dashboard</li>
+                        </ul>
+                    </nav>
+                </div>
 
-		<div class="popup-tabs-container">
+                <!-- Fun Facts Container -->
+                <div class="fun-facts-container">
+                    <div class="fun-fact" data-fun-fact-color="#36bd78">
+                        <div class="fun-fact-text">
+                            <span>Applicants</span>
+                            <h4>@php
+                                    $sum=0;
+                                   foreach( auth::user()->jobs as $job) {
+                                        $sum+= $job->applicants->count();
+                                   }
+                                   echo $sum;
+                                @endphp
+                            </h4>
+                        </div>
+                        <div class="fun-fact-icon"><i class="icon-material-outline-person-pin"></i></div>
+                    </div>
+                    <div class="fun-fact" data-fun-fact-color="#b81b7f">
+                        <div class="fun-fact-text">
+                            <span>Active Jobs</span>
+                            <h4>{{auth::user()->jobs->where('active',1)->count() }} </h4>
+                        </div>
+                        <div class="fun-fact-icon"><i class="icon-material-outline-business-center"></i></div>
+                    </div>
+                    <div class="fun-fact" data-fun-fact-color="#efa80f">
+                        <div class="fun-fact-text">
+                            <span>Pending Jobs</span>
+                            <h4>28</h4>
+                        </div>
+                        <div class="fun-fact-icon"><i class="icon-material-outline-rate-review"></i></div>
+                    </div>
+                </div>
 
-			<!-- Tab -->
-			<div class="popup-tab-content" id="tab">
-				
-				<!-- Welcome Text -->
-				<div class="welcome-text">
-					<h3>Do Not Forget 😎</h3>
-				</div>
-					
-				<!-- Form -->
-				<form method="post" id="add-note">
+                <!-- Row -->
+                <div class="row">
 
-					<select class="selectpicker with-border default margin-bottom-20" data-size="7" title="Priority">
-						<option>Low Priority</option>
-						<option>Medium Priority</option>
-						<option>High Priority</option>
-					</select>
+                    <!-- Dashboard Box -->
+                    <div class="col-xl-12">
+                        <div class="dashboard-box">
+                            <div class="headline">
+                                <h3><i class="icon-material-baseline-notifications-none"></i> Notifications</h3>
+                                <button class="mark-as-read ripple-effect-dark" data-tippy-placement="left"
+                                        title="Mark all as read">
+                                    <i class="icon-feather-check-square"></i>
+                                </button>
+                            </div>
+                            <div class="content">
+                                <ul class="dashboard-box-list">
+                                    @include('dashboard.notifications.dashboard')
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Row / End -->
+            </div>
+        </div>
+        <!-- Dashboard Content / End -->
 
-					<textarea name="textarea" cols="10" placeholder="Note" class="with-border"></textarea>
+    </div>
+    <!-- Dashboard Container / End -->
 
-				</form>
-				
-				<!-- Button -->
-				<button class="button full-width button-sliding-icon ripple-effect" type="submit" form="add-note">Add Note <i class="icon-material-outline-arrow-right-alt"></i></button>
+    </div>
+    <!-- Wrapper / End -->
 
-			</div>
 
-		</div>
-	</div>
-</div>
-<!-- Apply for a job popup / End -->
+    <!-- Apply for a job popup
+    ================================================== -->
+    <div id="small-dialog" class="zoom-anim-dialog mfp-hide dialog-with-tabs">
+
+        <!--Tabs -->
+        <div class="sign-in-form">
+
+            <ul class="popup-tabs-nav">
+                <li><a href="#tab">Add Note</a></li>
+            </ul>
+
+            <div class="popup-tabs-container">
+
+                <!-- Tab -->
+                <div class="popup-tab-content" id="tab">
+
+                    <!-- Welcome Text -->
+                    <div class="welcome-text">
+                        <h3>Do Not Forget 😎</h3>
+                    </div>
+
+                    <!-- Form -->
+                    <form method="post" id="add-note">
+
+                        <select class="selectpicker with-border default margin-bottom-20" data-size="7"
+                                title="Priority">
+                            <option>Low Priority</option>
+                            <option>Medium Priority</option>
+                            <option>High Priority</option>
+                        </select>
+
+                        <textarea name="textarea" cols="10" placeholder="Note" class="with-border"></textarea>
+
+                    </form>
+
+                    <!-- Button -->
+                    <button class="button full-width button-sliding-icon ripple-effect" type="submit" form="add-note">
+                        Add Note <i class="icon-material-outline-arrow-right-alt"></i></button>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Apply for a job popup / End -->
 @endsection
 
